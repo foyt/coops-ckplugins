e2dbb945a09e1ed563166e9556c4d1a9e13dcf7f
Antti Leppä <antti.leppa@foyt.fi>
2015-02-02 06:26:29 +0200

    ** Fixed a bug in coops plugin that caused plugin to listen for changes in readOnly mode ** Fixed a bug coops plugin that caused initial contents of the editor to be misinterpret

--- coops/plugin.js
+++ coops/plugin.js
@@ -9,11 +9,14 @@
       this._savedContent = null;
       this._rnrTimeout = null;
       
-      this._editor.on('contentChange', this._onContentChange, this);
+      if (this._editor.config.coops.readOnly !== true) {
+        this._editor.on('contentChange', this._onContentChange, this);
+        this._editor.on('propertiesChange', this._onPropertiesChange, this);
+      }
+      
       this._editor.on('CoOPS:SessionStart', this._onSessionStart, this);
       this._editor.on('CoOPS:ContentPatch', this._onContentPatch, this);
       this._editor.on('CoOPS:ContentRevert', this._onContentRevert, this);
-      this._editor.on('propertiesChange', this._onPropertiesChange, this);
       this._editor.on('CoOPS:PatchSent', this._onPatchSent, this);
       this._editor.on('CoOPS:PatchAccepted', this._onPatchAccepted, this);
       this._editor.on('CoOPS:PatchMerged', this._onPatchMerged, this);
@@ -85,7 +88,7 @@
       },
       
       _onSessionStart: function (event) {
-        this.setSavedContent(this.getUnsavedContent());
+        this.setSavedContent(event.data.content);
       },
       
       _onContentPatch: function (event) {
@@ -154,8 +157,11 @@
               this.getChangeObserver().reset(content);
               this.getChangeObserver().resume();
               this.setReadOnly(false);
-              this.fire("CoOPS:SessionStart");
             }
+              
+            this.fire("CoOPS:SessionStart", {
+              content: content
+            });
           });
         } else {
           this._editor.fire("CoOPS:Error", {
@@ -170,7 +176,9 @@
       },
       
       _onReconnect: function (event) {
-        this._editor.setReadOnly(false);
+        if (this._editor.config.coops.readOnly !== true) {
+          this._editor.setReadOnly(false);
+        }
       }
     }
   });
